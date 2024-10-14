d87b8808928eeac541a642b06406f95bcb17cd6d
Alejandro Rivera <141943838+alesito14@users.noreply.github.com>
2024-10-14 12:32:05 -0600

    Add files via upload

---
+++ ejemplo.java
@@ -0,0 +1,13 @@
+
+public class ejemplo{
+    public static void main(String[] args){
+
+        int num1 = 2;
+        int num2 = 4;
+
+        if (num1 < num2){
+            System.out.println("el num2 es mayor que el num1");
+        }
+        
+    }
+}
